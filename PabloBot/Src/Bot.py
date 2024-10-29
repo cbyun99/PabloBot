df454ead29e04299425bade0e2ba8924a40c517b
cbyun99 <cbyun99@gmail.com>
2024-10-28 19:07:50 -0700

    Bot Setup to send AI generated messages when image is sent in correct channel.  Result is not random

--- PabloBot/Src/Bot.py
+++ PabloBot/Src/Bot.py
@@ -1,32 +1,63 @@
-
-# bot.py
 import os
-import random
-
 import discord
+
 from dotenv import load_dotenv
+from discord.ext import commands
+from groq import Groq
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+API_KEY = os.getenv('API_KEY')
 
-client = discord.Client(intents=discord.Intents.default())
+ai = Groq(api_key=API_KEY)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@client.event
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+chat_completion = ai.chat.completions.create(
+    messages=[
+        {
+        "role": "user",
+        "content": ("Make a complement for Pablo the Husky by taking a random prompt from the list below"
+                    "and making a variation while keeping it informal. "
+                    "do not preface your response and only provide the single complement itself without quotes"
+                    "List is provided below"
+                    "Whos a good boyyy? Pablooo is!"
+                    "Pablo! The bestest boyyy!"
+                    "Ohhhh, look at this lil' cutie, Pablo!"
+                    "Who's the fluffiest floof? Thats right, its Pablo!"
+                    "Pablo, you handsome lil' pupper!"
+                    "Somebody give Pablo all the treats!"
+                    "Pablo, king of belly rubs!"
+                    "Smooches for Pablo, the sweetest pup!"
+                    "Who's got the cutest paws? Pablo does!"
+                    "Pablo, the tail-wagging champion!"
+                    ),
+        }
+    ],
+    model="llama3-8b-8192",
+    temperature=0.2,
+    top_p=0.9,
+   
+)
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.channel.id != CHANNEL_ID or message.author.bot:
         return
+    await bot.process_commands(message)
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+    if message.attachments:
+        for attachment in message.attachments:
+            if attachment.content_type.startswith("image/"):
+                await message.channel.send(chat_completion.choices[0].message.content)
+            return
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
 
-client.run(TOKEN)
+bot.run(TOKEN)
